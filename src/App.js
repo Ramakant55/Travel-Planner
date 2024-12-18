579fdfd88976b4f5db0edf8e81ad287620492150
Ramakant55 <jangirramakant786@gmail.com>
2024-12-18 22:39:34 +0530

    add hotels

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import { Route,BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
-import FeaturesSection from './Components/FeaturesSection';
-import PromoSection from './Components/PromoSection';
-import Try from './Components/Try';
-import TripPlanner from './Components/TripPlanner';
 import Footer from './Components/Footer';
+import Hotels from './Components/Hotels';
+import Homepage from './Components/Homepage';
 
 function App() {
   return (
     <>
-    <Router>
+     <Router>
       <Home/>
        <Routes>
+        <Route path='/' element={<Homepage/>}/>
          <Route path='login' element={<Login/>}/>
          <Route path='signup' element={<Signup/>}/>
+         <Route path='hotels' element={<Hotels/>}/>
       </Routes>
-    <TripPlanner/> 
+    
      </Router>
-    <Try/>
-    <FeaturesSection/>
-    <PromoSection/> 
-    <Footer/>
+   
+    <Footer/> 
+    
     </>
   );
 }
